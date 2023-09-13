9e6088c2c81d37d6c278b9a523dfdcbf03873099
elkhailihamza <elkhailihamza331@gmail.com>
2023-09-13 18:55:40 +0100

    Added history!

--- app.c
+++ app.c
@@ -6,21 +6,27 @@ int main() {
     char operator;
 
     double history[5];
+
+    for (int i = 0; i < 5; i++) {
+        history[i] = 0;
+    }
+    
     int option;
     int i = 0;
 
-    double num1;
-    double num2;
+    double num1, num2;
 
     double result;
 
-    while (1)
-    {   
-        while(i > 5) {
-            printf("\nEnter an equation: ");
-            scanf("%lf", &num1);
-            scanf(" %c", &operator);
-            scanf("  %lf", &num2);
+    while (1) {   
+        printf("\nSelect option: ");
+        scanf("%d", &option);
+        if (option == 1) {
+            while(i < 5) {
+                printf("\nEnter an equation: ");
+                scanf("%lf", &num1);
+                scanf(" %c", &operator);
+                scanf(" %lf", &num2);
 
             if(operator == operators[0]) {
                 result = num1 + num2;
@@ -46,9 +52,24 @@ int main() {
                 printf("Err");
                 return 1;
             }
-        printf("\n%2.lf %c%2.lf = %2.lf\n", num1, operator, num2, result);
-    }
+            printf("\n%.2lf %c %.2lf = %.2lf\n", num1, operator, num2, result);
+            option = 0;
+            }
+        } else if (option == 2) {
+            printf("\n\tHistory: \n");
+            for (int y = 0; y < 5; y++) {
+                printf("%d.\t%.2lf\n", y + 1, history[y]);
+                i--;
+            }
+            option = 0;
+        } else if(option == 3) {
+            printf("quit");
+            return 0;
+        } else {
+            printf("Err");
+            return 1;
         }
+    }
 
     return 0;
 }
